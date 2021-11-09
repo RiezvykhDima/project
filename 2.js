df393988bd921a0d22eb478217726656785bced3
TeslaNicola <rezvih12@gmail.com>
2021-11-09 14:22:22 +0200

    end

--- 2.js
+++ 2.js
@@ -1,10 +1,8 @@
-exports.fibonachi = function() {
-    // 0 1 1 2 3 5 8 13 21 34 55 89 }
-    // 0 1 1 2 4 7 13 24 44 
+exports.fibonachi = function(number) {
 
     let numbers = [0,1,];
 
-    for (let i = 2; i < 30; i++) {
+    for (let i = 2; i < number; i++) {
       let plus = numbers[i-1] + numbers[i-2];
       numbers.push(plus);
     }
